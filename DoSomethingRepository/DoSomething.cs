ad425215fe8faa9cb427c1de4c247f1306fcc1b9
mccains <mccains@pacden.com>
2018-09-13 08:48:32 -0700

    APPARCH-243 Hangfire plugin service

---
+++ DoSomethingRepository/DoSomething.cs
@@ -0,0 +1,17 @@
+﻿using HangfireService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoSomethingRepository
+{
+    public class DoSomething : IDoSomethingService
+    {
+        void IDoSomethingService.DoSomething()
+        {
+            return;
+        }
+    }
+}
---
+++ HangfireService.Plugins/AnotherPlugin.cs
@@ -0,0 +1,21 @@
+﻿using HangfireService.Contracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangfireService.Plugins
+{
+    [Export(typeof(IPluginHandler))]
+    [ExportMetadata("Name", "AnotherPlugin")]
+    public class AnotherPlugin : IPluginHandler
+    {
+        public async Task Handle()
+        {
+            Console.WriteLine("Inside Another Plugin");
+            
+        }
+    }
+}
